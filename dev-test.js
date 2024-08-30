c39ac676136b3a28aae3de18613e634710fa7a2e
Ja0n0aJ <jana.janzen1@web.de>
2024-08-30 10:30:42 +0000

    test.chain

--- dev-test.js
+++ dev-test.js
@@ -1,6 +1,8 @@
 
 // Import
 const Block = require("./block");
+const Blockchain = require("./blockchain");
+
 
 /***  Test der Block-Funktionalität  */
 // const block = new Block("aktuelle zeit","hash1","hash2","Blockdaten");
@@ -8,7 +10,19 @@ const Block = require("./block");
 
 // console.log(Block.genesis().toString()); // Ausgabe des Genesis-Blocks als String
 
-const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
-console.log(testBlock.toString());
+// const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
+// console.log(testBlock.toString());
 
+/***  Test der Blockchain-Funktionalität  */
+const chain = new Blockchain();
+console.log(chain);
+
+chain.addBlock("1034,98€ von Sven");
+console.log(chain);
+
+chain.addBlock("2069,87��� von Thomas");
+console.log(chain);
+
+chain.addBlock("20,00€ von Saba, 75.00e an Soraya");
+console.log(chain);
 
