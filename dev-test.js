932089d7e834bd1c9594cc2c8a08f8c7250b7978
Ja0n0aJ <jana.janzen1@web.de>
2024-08-30 08:42:23 +0000

    Klasse Block/ constructor

---
+++ dev-test.js
@@ -0,0 +1,9 @@
+
+// Import
+const Block = require("./block");
+
+/***  Test der Block-Funktionalität  */
+// const block = new Block("aktuelle zeit","hash1","hash2","Blockdaten");
+// console.log(block.toString()); // Ausgabe des Blocks als String
+
+console.log(Block.genesis().toString()); // Ausgabe des Genesis-Blocks als String
